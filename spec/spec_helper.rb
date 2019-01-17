4b104fc1829d7cb40d13831057c7cad89702508f
Anton Davydov <antondavydov.o@gmail.com>
2019-01-17 13:40:44 +0300

    Use vcr for stub http requests in specs

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,12 @@
 require "bundler/setup"
 require "safelylaunch"
+require 'vcr'
+
+VCR.configure do |config|
+  config.cassette_library_dir = "./spec/fixtures/vcr_cassettes"
+  config.hook_into :webmock
+  config.configure_rspec_metadata!
+end
 
 RSpec.configure do |config|
   # Enable flags like --only-failures and --next-failure
--- spec/unit/http_connection_spec.rb
+++ spec/unit/http_connection_spec.rb
@@ -1,20 +1,23 @@
 require 'logger'
 
 RSpec.describe Safelylaunch::HttpConnection do
-  let(:conn) { described_class.new(api_token: api_token) }
+  let(:conn) { described_class.new(api_token: api_token, logger: logger) }
+  let(:logger) { Logger.new(StringIO.new) }
   let(:api_token) { '970144d3-fa42-42ca-93d3-b911e0282c14' }
   let(:response) { double(:response, body: {}) }
 
   describe '#get' do
-    it { expect(conn.get('stream-toggle')).to eq(key: "stream-toggle", enable: true) }
-    it { expect(conn.get('stream-toggle')).to eq(conn.get('stream-toggle')) }
+    context 'when toggle exist', vcr: { cassette_name: 'toggle-exist' } do
+      it { expect(conn.get('stream-toggle')).to eq(key: "stream-toggle", enable: true) }
+      it { expect(conn.get('stream-toggle')).to eq(conn.get('stream-toggle')) }
 
-    it do
-      expect(conn.connection).to receive(:get).once.and_return(response)
-      conn.get('stream-toggle')
+      it do
+        expect(conn.connection).to receive(:get).once.and_return(response)
+        conn.get('stream-toggle')
 
-      expect(conn.connection).to_not receive(:get)
-      conn.get('stream-toggle')
+        expect(conn.connection).to_not receive(:get)
+        conn.get('stream-toggle')
+      end
     end
   end
 end
